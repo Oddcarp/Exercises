8caaed9263706e4c147e5c5c3d9d27f8ad6dc51c
Mckenzie Keller <Mckenzie.Keller@jbhunt.com>
2018-03-28 12:28:55 -0500

    Exercise14 adding

--- Exercises/Exercise14/scripts/main.js
+++ Exercises/Exercise14/scripts/main.js
@@ -1,87 +1,93 @@
-
 function reset() {
-    getElm('userName').value = "";
-    getElm('password').value = "";
-    getElm('emailAddress').value = "";
-    getElm('timeZone').selectedIndex = "0"
-    getElm('error-list').innerHTML = "";
+  getElm('userName').value = "";
+  getElm('password').value = "";
+  getElm('emailAddress').value = "";
+  getElm('timeZone').selectedIndex = "0"
+  getElm('error-list').innerHTML = "";
 
 
-    if (getElm('userName').classList.contains('invalid')) {
-        getElm('userName').classList.remove('invalid');
-      }
-      if (getElm('password').classList.contains('invalid')) {
-        getElm('password').classList.remove('invalid');
-      }
-      if (getElm('emailAddress').classList.contains('invalid')) {
-        getElm('emailAddress').classList.remove('invalid');
-      }
-      if (getElm('timeZone').classList.contains('invalid')) {
-          getElm('timeZone').classList.remove('invalid');
-        }
+
+  if (getElm('userName').classList.contains('invalid')) {
+    getElm('userName').classList.remove('invalid');
+  }
+  if (getElm('password').classList.contains('invalid')) {
+    getElm('password').classList.remove('invalid');
+  }
+  if (getElm('emailAddress').classList.contains('invalid')) {
+    getElm('emailAddress').classList.remove('invalid');
+  }
+  if (getElm('timeZone').classList.contains('invalid')) {
+    getElm('timeZone').classList.remove('invalid');
+  }
+  if (getElm('terms').checked) {
+    getElm('terms').checked = false;
+  }
 
 }
 
 
 function update() {
-    //////////////////////Your Code Starts here
-    if (getElm('userName').classList.contains('invalid')) {
-      getElm('userName').classList.remove('invalid');
-    }
-    if (getElm('password').classList.contains('invalid')) {
-      getElm('password').classList.remove('invalid');
-    }
-    if (getElm('emailAddress').classList.contains('invalid')) {
-      getElm('emailAddress').classList.remove('invalid');
-    }
-    if (getElm('timeZone').classList.contains('invalid')) {
-        getElm('timeZone').classList.remove('invalid');
-      }
-
-    getElm('error-list').innerHTML = "";
-    var userName = getValue('userName');
-    var emailAddress = getValue('emailAddress');
-    var password = getValue('password');
-    var timeZone = getValue('timeZone');
-    var messages = [];
-    console.log(timeZone);
-
-    //User Name
-    if (userName === "") {
-      getElm('userName').classList.add('invalid');
-      messages.push("Last Name is required")
-    }
-    
+  //////////////////////Your Code Starts here
+  if (getElm('userName').classList.contains('invalid')) {
+    getElm('userName').classList.remove('invalid');
+  }
+  if (getElm('password').classList.contains('invalid')) {
+    getElm('password').classList.remove('invalid');
+  }
+  if (getElm('emailAddress').classList.contains('invalid')) {
+    getElm('emailAddress').classList.remove('invalid');
+  }
+  if (getElm('timeZone').classList.contains('invalid')) {
+    getElm('timeZone').classList.remove('invalid');
+  }
 
 
-    // Email
-    var emailPattern = new RegExp("@"); 
-    if (!emailPattern.test(emailAddress)) {
-      getElm('emailAddress').classList.add('invalid');
-      messages.push("A valid email address is required")
-    }
+  getElm('error-list').innerHTML = "";
+  var userName = getValue('userName');
+  var emailAddress = getValue('emailAddress');
+  var password = getValue('password');
+  var timeZone = getValue('timeZone');
+  var messages = [];
+  console.log(timeZone);
+
+  //User Name
+  if (userName === "") {
+    getElm('userName').classList.add('invalid');
+    messages.push("Last Name is required");
+  }
+
+  // Email
+  var emailPattern = new RegExp("@");
+  if (!emailPattern.test(emailAddress)) {
+    getElm('emailAddress').classList.add('invalid');
+    messages.push("A valid email address is required");
+  }
+
+  // Password
+  if (password === "") {
+    getElm('password').classList.add('invalid');
+    messages.push("Password is required");
+  }
+
+  // Timezone
+  if (timeZone === "0") {
+    getElm('timeZone').classList.add('invalid');
+    messages.push("Please select a time zone");
+  }
+
+  // Terms
+  if (!getElm('terms').checked) {
+    messages.push("You must accept the terms and conditions");
+  }
 
 
-    // Password
-    if (password === "") {
-        getElm('password').classList.add('invalid');
-        messages.push("Password is required")
-    }
+  if (messages == "") {
+    isValid = true;
+  }
 
-    // Timezone
-    if (timeZone === "0") {
-        getElm('timeZone').classList.add('invalid');
-        messages.push("Please select a time zone")
-    }
-
-
-    if (messages == "") {
-        isValid = true;
-    }  
-
-    for (i=0;i<messages.length;i++) {
-        getElm('error-list').innerHTML += messages[i] + "<br>";
-    }
+  for (i = 0; i < messages.length; i++) {
+    getElm('error-list').innerHTML += messages[i] + "<br>";
+  }
 }
 
 
@@ -92,11 +98,11 @@ btn.addEventListener('click', update);
 btnReset.addEventListener('click', reset);
 
 function getValue(id) {
-    return getElm(id).value;
+  return getElm(id).value;
 }
 
 function getElm(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function isNumeric(n) {
