8b79e66aeabd19f663835ac2cacb29a20c75a38e
Ray <mkeller1991@gmail.com>
2018-03-29 18:09:02 -0500

    Assignment 2 tests

--- Exercises/Assignment2/DateFormatting.js
+++ Exercises/Assignment2/DateFormatting.js
@@ -10,28 +10,27 @@ var DateFormatter = {
         var shortTime = hours + ':' + minutes + ' ' + amOrPM;
         return shortTime;
     },
-    
+
     getLongTime: function(d){
          var hours = d.getHours();
         var minutes = d.getMinutes();
-        var amOrPM = hours >= 12 ? "PM" : "AM"; 
-        var seconds = d.getSeconds();  
+        var amOrPM = hours >= 12 ? "PM" : "AM";
+        var seconds = d.getSeconds();
         hours = hours % 12;
         hours = hours ? hours : 12;
         var longTime = hours + ':' + minutes + ':' + seconds + ' ' + amOrPM;
         return longTime;
     },
-    
+
     getShortDate: function(d){
         var months = d.getMonth();
         var days = d.getDate();
         var year = d.getFullYear();
         months++;
-        days++;
         var shortDate = months + '/' + days + '/' + year;
         return shortDate;
     },
-    
+
     getLongDate: function(d){
         var months = d.getMonth();
         var monthNameArray = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -41,17 +40,17 @@ var DateFormatter = {
         var longDate = monthName + ' ' + days + ', ' + year;
         return longDate;
     },
-    
+
     getShortDateTime: function(d){
         var shortDateTime = this.getShortDate(d) + ' ' + this.getShortTime(d);
         return shortDateTime;
     },
-    
+
     getLongDateTime: function(d) {
         var longDateTime = this.getLongDate(d) + ' ' + this.getShortTime(d);
         return longDateTime;
     },
-    
+
     getExtendedDateTime: function(d) {
         var weekday = d.getDay();
         var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -59,10 +58,19 @@ var DateFormatter = {
         var extendedDateTime = weekdayActual + ', ' + this.getLongDateTime(d);
         return extendedDateTime;
     }
-    
-    
+
+
     };
-    
+/*
+    var x = new Date("2004/03/04 16:11:17");
+    console.log(DateFormatter.getShortTime(x));
+    console.log(DateFormatter.getLongTime(x));
+    console.log(DateFormatter.getShortDate(x));
+    console.log(DateFormatter.getLongDate(x));
+    console.log(DateFormatter.getShortDateTime(x));
+    console.log(DateFormatter.getLongDateTime(x));
+    console.log(DateFormatter.getExtendedDateTime(x));
+*/
     // var d = new Date("October 13, 2014 11:13:00");
     // 11:13 AM
     // 11:13:0 AM
@@ -71,6 +79,3 @@ var DateFormatter = {
     // 10/14/2014 11:13 AM
     // October 13, 2014 11:13 AM
     // Monday, October 13, 2014 11:13 AM
-    
-    
-    
--- Exercises/Assignment2/DateFormatting.tests.js
+++ Exercises/Assignment2/DateFormatting.tests.js
@@ -1,76 +1,118 @@
 
-var DateFormatter = {
 
-    getShortTime: function(d){
-        var hours = d.getHours();
-        var minutes = d.getMinutes();
-        var amOrPM = hours >= 12 ? "PM" : "AM";
-        hours = hours % 12;
-        hours = hours ? hours : 12;
-        var shortTime = hours + ':' + minutes + ' ' + amOrPM;
-        return shortTime;
-    },
-    
-    getLongTime: function(d){
-         var hours = d.getHours();
-        var minutes = d.getMinutes();
-        var amOrPM = hours >= 12 ? "PM" : "AM"; 
-        var seconds = d.getSeconds();  
-        hours = hours % 12;
-        hours = hours ? hours : 12;
-        var longTime = hours + ':' + minutes + ':' + seconds + ' ' + amOrPM;
-        return longTime;
-    },
-    
-    getShortDate: function(d){
-        var months = d.getMonth();
-        var days = d.getDate();
-        var year = d.getFullYear();
-        months++;
-        days++;
-        var shortDate = months + '/' + days + '/' + year;
-        return shortDate;
-    },
-    
-    getLongDate: function(d){
-        var months = d.getMonth();
-        var monthNameArray = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var monthName = monthNameArray[months];
-        var days = d.getDate();
-        var year = d.getFullYear();
-        var longDate = monthName + ' ' + days + ', ' + year;
-        return longDate;
-    },
-    
-    getShortDateTime: function(d){
-        var shortDateTime = this.getShortDate(d) + ' ' + this.getShortTime(d);
-        return shortDateTime;
-    },
-    
-    getLongDateTime: function(d) {
-        var longDateTime = this.getLongDate(d) + ' ' + this.getShortTime(d);
-        return longDateTime;
-    },
-    
-    getExtendedDateTime: function(d) {
-        var weekday = d.getDay();
-        var weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var weekdayActual = weekdayArray[weekday];
-        var extendedDateTime = weekdayActual + ', ' + this.getLongDateTime(d);
-        return extendedDateTime;
+var dateTest1 = new Date("2010/08/17 12:09:36");
+var dateTest2 = new Date("2004/03/04 16:11:17");
+
+
+
+  function compareDateShortTest1(d) {
+    if ("8/17/2010" === DateFormatter.getShortDate(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
     }
-    
-    
-    };
-    
-    
-    // 11:13 AM
-    // 11:13:0 AM
-    // 10/14/2014
-    // October 13, 2014
-    // 10/14/2014 11:13 AM
-    // October 13, 2014 11:13 AM
-    // Monday, October 13, 2014 11:13 AM
-    
-    
-    
+  }
+
+  function compareDateShortTest2(d) {
+    if ("3/5/2004" === DateFormatter.getShortDate(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareShortTimeTest1(d) {
+    if ("12:9 PM" === DateFormatter.getShortTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareShortTimeTest2(d) {
+    if ("4:11 PM" === DateFormatter.getShortTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareLongTimeTest1(d) {
+    if ("12:9:36 PM" === DateFormatter.getLongTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareLongTimeTest2(d) {
+    if ("4:11:17 PM" === DateFormatter.getLongTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareDateLongTest1(d) {
+    if ("August 17, 2010" === DateFormatter.getLongDate(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareDateLongTest2(d) {
+    if ("March 4, 2004" === DateFormatter.getLongDate(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareShortDateTimeTest1(d) {
+    if ("8/17/2010 12:9 PM" === DateFormatter.getShortDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareShortDateTimeTest2(d) {
+    if ("3/5/2004 4:11 PM" === DateFormatter.getShortDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareLongDateTimeTest1(d) {
+    if ("August 17, 2010 12:9 PM" === DateFormatter.getLongDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareLongDateTimeTest2(d) {
+    if ("March 4, 2004 4:11 PM" === DateFormatter.getLongDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareExtendedDateTimeTest1(d) {
+    if ("Tuesday, August 17, 2010 12:9 PM" === DateFormatter.getExtendedDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
+
+  function compareExtendedDateTimeTest2(d) {
+    if ("Thursday, March 4, 2004 4:11 PM" === DateFormatter.getExtendedDateTime(d)) {
+      console.log("Success");
+    } else {
+      throw "Fail";
+    }
+  }
