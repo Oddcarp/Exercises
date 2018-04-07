b5133b80b1f9ed30ac80947dcc7592bf8989bbed
Ray <mkeller1991@gmail.com>
2018-04-07 11:15:52 -0500

    ex 15&16

--- Exercises/Exercise15/scripts/main.js
+++ Exercises/Exercise15/scripts/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
 
      ///// Code goes here
+  $('#btnOne').click(function(){
+    alert("The button was clicked.");
+  });
 
+  $('#btnTwo').click(function(){
+    $(".update-html").html('<h3>Hello Word!</h3>');
+  });
 
+  $('#btnThree').click(function(){
+    $("#text-update").val('<h3>Hello Word!</h3>');
+  });
 
+  $('#btnCopyHtml').click(function(){
+    var copiedHtml = $('.copy-html').html();
+    console.log(copiedHtml);
+    $('.paste-html').html(copiedHtml);
+  });
 
+  $('#btnCopyValues').click(function(){
+    var copiedValue = $("#copy-text").val();
+    $("#paste-text").val(copiedValue);
+  });
 
 
 });
--- Exercises/Exercise16/scripts/main.js
+++ Exercises/Exercise16/scripts/main.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
 
+  $('#btnShow').click(function() {
+    $('.display-basic').show();
+  });
 
+  $('#btnHide').click(function() {
+    $('.display-basic').hide();
+  });
+
+  $('#btnToggle').click(function() {
+    $('.display-basic').toggle();
+  });
+
+
+
+  $('#btnFadeIn').click(function() {
+    $('.display-fade').fadeIn();
+  });
+
+  $('#btnFadeOut').click(function() {
+    $('.display-fade').fadeOut();
+  });
+
+  $('#btnFadeToggle').click(function() {
+    $('.display-fade').fadeToggle();
+  });
+
+
+
+  $('#btnSlideDown').click(function() {
+    $('.display-slide').slideDown();
+  });
+
+  $('#btnSlideUp').click(function() {
+    $('.display-slide').slideUp();
+  });
+
+  $('#btnSlideToggle').click(function() {
+    $('.display-slide').slideToggle();
+  });
 
 });
+
+$('.part3').show();
