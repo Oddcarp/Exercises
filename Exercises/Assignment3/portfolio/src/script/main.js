939249f1241338821621234e277ecc5292ab25d9
Ray <mkeller1991@gmail.com>
2018-04-17 17:18:16 -0500

    Assignment 3

---
+++ Exercises/Assignment3/portfolio/src/script/main.js
@@ -0,0 +1,12 @@
+
+
+const jquery = require("jquery");
+const modernizr = require("modernizr");
+
+$( document ).ready(function() {
+    $( '#container' ).SecretNav({
+    	navSelector: 'nav',			// selector of the nav tag
+		openSelector: '.open-menu',	// selector of the menu's opener
+		position: 'left'			// left | top
+    });
+});
