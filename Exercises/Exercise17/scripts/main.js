7a715d92b2b4ac5a427788813bb6f54fba0f111e
Ray <mkeller1991@gmail.com>
2018-04-17 18:33:03 -0500

    fix 17

--- Exercises/Exercise17/scripts/main.js
+++ Exercises/Exercise17/scripts/main.js
@@ -7,25 +7,25 @@ $(document).ready(function () {
     $('#firstName').find('span').show();
     $('#firstName').blur(function () {
         if ($('#firstName').val() === '') {
-            $('span:first').show();
+            $('span:first').fadeIn();
         } else {
-            $('span:first').hide();
+            $('span:first').fadeOut();
         };
     });
 
     $('#lastName').blur(function () {
         if ($('#lastName').val() === '') {
-            $('.input-group:nth-child(2) span').show();
+            $('.input-group:nth-child(2) span').fadeIn();
         } else {
-            $('.input-group:nth-child(2) span').hide();
+            $('.input-group:nth-child(2) span').fadeOut();
         };
     });
 
     $('#emailAddress').blur(function () {
         if ($('#emailAddress').val() === '') {
-            $('.input-group:nth-child(3) span').show();
+            $('.input-group:nth-child(3) span').fadeIn();
         } else {
-            $('.input-group:nth-child(3) span').hide();
+            $('.input-group:nth-child(3) span').fadeOut();
         };
         let testEmail = new RegExp("@");
         let testEmail2 = new RegExp(".com");
@@ -33,9 +33,9 @@ $(document).ready(function () {
         console.log(emailAddress)
         console.log(testEmail.test(emailAddress));
         if (testEmail.test(emailAddress) === false || testEmail2.test(emailAddress) === false) {
-            $('.emailMsg').show();
+            $('.emailMsg').fadeIn();
         } else {
-            $('.emailMsg').hide();
+            $('.emailMsg').fadeOut();
         }
     });
 
