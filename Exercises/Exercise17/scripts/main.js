c042a8d4e267f1f03faa7bde8a46cbbb00e81e8b
Ray <mkeller1991@gmail.com>
2018-04-17 18:26:00 -0500

    assignment 17

--- Exercises/Exercise17/scripts/main.js
+++ Exercises/Exercise17/scripts/main.js
@@ -1,5 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    $('.error').hide();
 
+    let inputsEven = $('.input-group').filter(':even');
+    inputsEven.addClass('even');
+    $('#firstName').find('span').show();
+    $('#firstName').blur(function () {
+        if ($('#firstName').val() === '') {
+            $('span:first').show();
+        } else {
+            $('span:first').hide();
+        };
+    });
+
+    $('#lastName').blur(function () {
+        if ($('#lastName').val() === '') {
+            $('.input-group:nth-child(2) span').show();
+        } else {
+            $('.input-group:nth-child(2) span').hide();
+        };
+    });
+
+    $('#emailAddress').blur(function () {
+        if ($('#emailAddress').val() === '') {
+            $('.input-group:nth-child(3) span').show();
+        } else {
+            $('.input-group:nth-child(3) span').hide();
+        };
+        let testEmail = new RegExp("@");
+        let testEmail2 = new RegExp(".com");
+        let emailAddress = $('#emailAddress').val();
+        console.log(emailAddress)
+        console.log(testEmail.test(emailAddress));
+        if (testEmail.test(emailAddress) === false || testEmail2.test(emailAddress) === false) {
+            $('.emailMsg').show();
+        } else {
+            $('.emailMsg').hide();
+        }
+    });
 
 });
