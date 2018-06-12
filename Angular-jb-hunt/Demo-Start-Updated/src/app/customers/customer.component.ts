00784f001959c4ca2f190d70bccbc11091a7e775
jisamk1 <McKenzie.Keller@Jbhunt.com>
2018-06-12 12:43:41 -0500

    Angular reactive forms update

--- Angular-jb-hunt/Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Angular-jb-hunt/Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,7 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 
 import { Customer } from './customer';
+import 'rxjs/add/operator/debounceTime';
+
+
+
+function emailMatcher(c: AbstractControl) {
+    let emailControl = c.get('email');
+    let confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return {'match':true};
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return {'range':true};
+        };
+    return null;
+    };
+}
 
 @Component({
     selector: 'my-signup',
@@ -10,21 +34,46 @@ import { Customer } from './customer';
 export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
     customer: Customer= new Customer();
+    emailMessage: string;
+
+    private validationMessages = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address'
+    };
 
     constructor(private fb: FormBuilder) {
     }
     
     ngOnInit() {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: '',
-            email: '',
+            firstName: ['', [Validators.required,Validators.minLength(3)]],
+            lastName: ['', [Validators.required,Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['',[Validators.required,Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+                confirmEmail: ['', Validators.required],
+            }, {validator: emailMatcher}),            
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1,5)],
             sendCatalog:true
         });
+
+        this.customerForm.get('notification').valueChanges
+                .subscribe(value => this.setNotification(value));
+
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.debounceTime(1000).subscribe(value => this.setMessage(emailControl));
     }
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage='';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key=> this.validationMessages[key]).join(' ');
+        }
     }
 
     populateTestData() {
@@ -33,5 +82,16 @@ export class CustomerComponent implements OnInit {
             lastName: 'Harkness',
             sendCatalog: false
         })
+    }
+
+        
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text' ) {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
  }
